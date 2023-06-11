8c3379f098fac53d117c476d7839f8f7c9119e10
Nikita Goncharenko <nikita.122495@gmail.com>
2023-06-11 22:08:00 +0200

    changed server.js serving of frontend

--- backend/server.js
+++ backend/server.js
@@ -17,4 +17,16 @@ app.use("/api/users", require("./routes/userRoutes"));
 
 app.use(errorHandler);
 
+// Default Index Page
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "/client/build/index.html"))
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
+
 app.listen(port, () => console.log(`Server running on the port ${port}`));
